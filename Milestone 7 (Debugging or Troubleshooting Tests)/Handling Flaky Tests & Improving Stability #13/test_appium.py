1df0a978f28decb482ae9dc9b25f2c1bf663b29c
Klinnsonveins Yee <19103507@usc.edu.ph>
2025-03-14 10:16:54 +0800

    Add implicit wait for issue #13

---
+++ Milestone 7 (Debugging or Troubleshooting Tests)/Handling Flaky Tests & Improving Stability #13/test_appium.py
@@ -0,0 +1,14 @@
+from appium import webdriver
+from appium.options.windows import WindowsOptions
+
+options = WindowsOptions()
+options.app = "notepad.exe"
+
+driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
+
+driver.implicitly_wait(10)
+
+text_area = driver.find_element("class name", "RichEditD2DPT")
+text_area.send_keys("Hello, World!")
+
+driver.quit()
