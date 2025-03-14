ef531aab22ee31b4ee0c2e4578efed168e239860
Klinnsonveins Yee <19103507@usc.edu.ph>
2025-03-14 10:17:46 +0800

    Add explicit wait for issue #13

--- Milestone 7 (Debugging or Troubleshooting Tests)/Handling Flaky Tests & Improving Stability #13/test_appium.py
+++ Milestone 7 (Debugging or Troubleshooting Tests)/Handling Flaky Tests & Improving Stability #13/test_appium.py
@@ -1,14 +1,17 @@
 from appium import webdriver
 from appium.options.windows import WindowsOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 options = WindowsOptions()
 options.app = "notepad.exe"
 
 driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
 
-driver.implicitly_wait(10)
+wait = WebDriverWait(driver, 30)
+text_area = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "RichEditD2DPT")))
 
-text_area = driver.find_element("class name", "RichEditD2DPT")
 text_area.send_keys("Hello, World!")
 
 driver.quit()
